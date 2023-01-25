40117d12d122778ef6f40f2cb2760d0e7395ae54
Nova Thomas <97986758+nova-thomas@users.noreply.github.com>
2023-01-25 17:10:54 -0500

    Outline
    Outlined the singly linked list API

--- singlyLinkedList.cpp
+++ singlyLinkedList.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+// Node Template (struct)
+    /*
+    VARIABLES
+    TYPE data;
+    node *next;
+    */
+
+// List Template
+    /*
+    VARIABLES
+    - Private:
+      - node *head, *tail;
+    CONSTRUCTORS
+    - default constructor
+    METHODS
+    - create node
+    - display
+    - pushFront (add to front)
+    - topFront (return front item)
+    - popFront (remove front item)
+    - pushBack (add to back)
+    - topBack (return back item)
+    - popBack (return back item)
+    - Boolean find (is key in list)
+    - erase (remove key from list
+    - Boolean empty (is it an empty list)
+    - addBefore(Node, Key) (adds key before node)
+    - addAfter(Node, Key) (adds key after node)
+    */
