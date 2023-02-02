00c6811592b5cb2ff907e76ea046c3510f238a15
Nova Thomas <97986758+nova-thomas@users.noreply.github.com>
2023-02-01 19:22:10 -0500

    Programmed most of the API
    I was able to program through most of the API functions so far. Still need to work on the ones that iterate through the functions. Had to copy and paste this commit, because I'm still trying to work out the commiting on Visual Studio.

--- singlyLinkedList.cpp
+++ singlyLinkedList.cpp
@@ -1,37 +1,115 @@
+/*
+* Nova Thomas
+* 2/2/2023
+* Singly Linked List API
+*/
+
 #include <iostream>
 
-// Node Template (struct)
-    /*
-    VARIABLES
-    TYPE data; (Key in node)
-    node *next;
-    */
+// Struct for a node in the list, holding the data and the address of next node
+struct node {
+    int data;
+    node* next;
+};
 
-// List Template
-    /*
-    VARIABLES
-    - Private:
-      - node *head, *tail;
-    CONSTRUCTORS
-    - default constructor
-    METHODS
-    - create node (not available to user)
-    - pushFront (add to front)
-    - topFront (return front item)
-    - popFront (remove front item)
-    - pushBack (add to back)
-    - topBack (return back item)
-    - popBack (return back item)
-    - Boolean find (is key in list)
-    - Boolean empty (is it an empty list)
-    - addBefore(Node, Key) (adds key before node)
-    - addAfter(Node, Key) (adds key after node)
-    - displayAll()
-    - size() - returns the size of the list
-    - replaceKey(Node, key) - overwrite the key given to a node
-    
-    notes: 
-    if a function is trying to add to end or beginning of an empty list, make a case for making the first node
-    populate list with push back and push front
-    when you add a new node, always update the tail and head (head only for adding node to beginning)
-    */
+class linkedList {
+private:
+    // Pointer to the head and tail of the list
+    node* head, * tail;
+
+public:
+    // Constructor
+    linkedList() {
+        head = NULL;
+        tail = NULL;
+    }
+
+    // Add a node to the front
+    void pushFront(int key) {
+        node* temp = new node;
+        temp->data = key;
+        temp->next = head;
+        head = temp;
+    }
+
+    // Return the front item
+    int topFront() {
+        return head->data;
+    }
+
+    // Remove the front item
+    void popFront() {
+        node* temp = new node;
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    // Add a node to the back
+    void pushBack(int key) {
+        node* temp = new node;
+        temp->data = key;
+        temp->next = NULL;
+        tail->next = temp;
+        tail = temp;
+    }
+
+    // Return the back item
+    int topBack() {
+        return tail->data;
+    }
+
+    // Remove the back item
+    void popBack() {
+        
+    }
+
+    // Searching the list to see if a given key is in the list
+    bool find(int key) {
+        // Loop through and check if there is a value matches the key
+
+    }
+
+    // Checking to see if the list is empty
+    bool empty() {
+        if (head == NULL && tail == NULL) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // Adds a key before a given node
+    void addBefore(int node, int key) {
+
+    }
+
+    // Adds a key after a given node
+    void addAfter(int node, int key) {
+
+    }
+
+    // Prints all of the elements of the list
+    void displayAll() {
+        // Loop to go through list printing each value
+    }
+
+    // Returns the number of elements in the list
+    int size() {
+        int count = 0;
+        // Loop to go through list starting at head
+
+        return count;
+    }
+
+    // Overwrites the data in a node with a given key
+    void replaceKey(int pos, int key) {
+        node* current = new node;
+        node* previous = new node;
+        current = head;
+
+        if (pos < this->size)
+        
+    }
+};
